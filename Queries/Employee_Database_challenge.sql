4c4a231595397b9c31b601548d81f11357d88011
Anthony Do <Anthonydo888@gmail.com>
2022-08-23 21:36:55 -0600

    Finish Code

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,9 @@
 SELECT 	e.emp_no,
-		e.first_name,
-		e.last_name,
-		ti.title,
-		ti.from_date,
-		ti.to_date
+e.first_name,
+e.last_name,
+ti.title,
+ti.from_date,
+ti.to_date
 INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
@@ -26,3 +26,20 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+dep.from_date,
+dep.to_date,
+ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as dep
+ON (e.emp_no = dep.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (dep.to_date = ('9999-01-01')) 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
